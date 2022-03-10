b3589d680ca87ad2d20ba3520d1b362c019bb71e
Sandeep.D <sandeep.d@simplilearn.net>
2022-03-10 22:22:27 +0530

    Commiting sum of two numbers to branch text

---
+++ src/Sumofnumbers.java
@@ -0,0 +1,18 @@
+
+public class Sumofnumbers {
+
+	public static void main(String[] args) {
+		int a =20,b=25;
+		int sum=0;
+		sum=a+b;
+		System.out.println("Enter the value of a\n"+a);
+		System.out.println("Enter the value of b\n"+b);
+		System.out.println("The sum of two numbers a and b" +sum);
+		
+		
+		
+		
+
+	}
+
+}
